0507a02311d750509223c0b1559e02a2930760ab
YashShrivastava10 <shrivastavayash10@gmail.com>
2024-12-07 10:57:45 +0530

    Re-generate sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,12 @@ import ViteSitemap from "vite-plugin-sitemap";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), ViteSitemap()],
+  plugins: [
+    react(),
+    ViteSitemap({
+      hostname: "https://portfolio-yash-shrivastava.vercel.app",
+    }),
+  ],
   optimizeDeps: {
     exclude: ["lucide-react"],
   },
